1fb914cac266ba8c1a2dda16fd8bf7f0585b5de2
Mohamed Emad <71214502+Mohamed-Emad1@users.noreply.github.com>
2024-09-21 00:51:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+function scrollToServices() {
+  var service = document.getElementById("services");
+  if (service) {
+    service.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+
+function scrollToAgents() {
+  var agents = document.getElementById("agents");
+  if (agents) {
+    agents.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+
+function scrollToContact() {
+  var contact = document.getElementById("contact");
+  if (contact) {
+    contact.scrollIntoView({ behavior: "smooth" });
+  }
+}
